208530b1a183a0b05b0ae2f4ddabfee5ac5c3726
Edwin Cacuango <edwin-fernando@hotmail.es>
2023-01-09 03:17:34 -0500

    Added picker app to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import PickerApp from "./components/PickerApp";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <PickerApp />
+    </div>
+  );
 }
 
 export default App;
