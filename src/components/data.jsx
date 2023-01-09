f2a189e5d371e559af51dd94cf5189d5a442fd50
Edwin Cacuango <edwin-fernando@hotmail.es>
2023-01-09 01:23:21 -0500

    Added a emoji list to use in the picker

---
+++ src/components/data.jsx
@@ -0,0 +1,22 @@
+export const data = [
+  {
+    symbol: "😊",
+    name: "Smiley face",
+    keywords: "happy smile smiling",
+  },
+  {
+    symbol: "😡",
+    name: "Angry face",
+    keywords: "red angry",
+  },
+  {
+    symbol: "❤️",
+    name: "Heart",
+    keywords: "love like heart",
+  },
+  {
+    symbol: "🤪",
+    name: "Crazy face",
+    keywords: "crazy funny clown",
+  },
+];
